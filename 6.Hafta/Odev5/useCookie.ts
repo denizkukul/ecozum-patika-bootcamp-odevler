a77b33b98ae191a0f47d67e8b4ecdc99dda05915
Deniz Kukul <deniz_kukul@hotmail.com>
2022-03-16 20:53:21 +0300

    Update useCookie.ts

--- 6.Hafta/Odev5/useCookie.ts
+++ 6.Hafta/Odev5/useCookie.ts
@@ -1,8 +1,10 @@
 import { useCallback } from "react"
 
-export const useCookie = (cookieName: 'string') => {
+export const useCookie = () => {
+
+  // Return value of cookieName or ''
   const getCookie = useCallback((cookieName: string) => {
-    document.cookie.match('(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)')?.pop() || ''
+    return document.cookie.match('(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)')?.pop() || ''
   }, [])
 
   const setCookie = useCallback((cookieName: string, value: string) => {
@@ -16,3 +18,9 @@ export const useCookie = (cookieName: 'string') => {
 
   return { getCookie, setCookie, deleteCookie };
 }
+
+// const { getCookie, setCookie, deleteCookie } = useCookie();
+// setCookie('cookie1', 'value1') adds cookie1=value1 to document.cookie
+// getCookie('cookie1') returns 'value1'
+// deleteCookie('cookie1') deletes cookie1=value1 from document.cookie
+// setCookie('cookie1', 'value2') updates cookie1=value1 to cookie1=value2
