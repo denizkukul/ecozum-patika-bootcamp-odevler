9614f86257a76c91db77f26e20df60af80475e4a
Deniz Kukul <deniz_kukul@hotmail.com>
2022-03-16 19:59:35 +0300

    Update useCookie.ts

--- 6.Hafta/Odev5/useCookie.ts
+++ 6.Hafta/Odev5/useCookie.ts
@@ -1,17 +1,17 @@
 import { useCallback } from "react"
 
 export const useCookie = (cookieName: 'string') => {
-  const getCookie = useCallback((name: string) => {
-    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+  const getCookie = useCallback((cookieName: string) => {
+    document.cookie.match('(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)')?.pop() || ''
   }, [])
 
-  const setCookie = useCallback((name: string, value: string) => {
-    document.cookie = `${name}=${value}`;
+  const setCookie = useCallback((cookieName: string, value: string) => {
+    document.cookie = `${cookieName}=${value}`;
   }, [])
 
-  const deleteCookie = useCallback((name: string) => {
+  const deleteCookie = useCallback((cookieName: string) => {
     // Set expiration date to past for browser to remove cookie
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+    document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
   }, [])
 
   return { getCookie, setCookie, deleteCookie };
